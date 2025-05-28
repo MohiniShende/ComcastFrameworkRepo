22f734af99c24deee1060504ec5d912c38a39b79
mohin <mohin@hp>
2025-05-28 13:13:10 +0530

    produact file added

--- ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectrepository/Product.java
+++ ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectrepository/Product.java
@@ -1,5 +1,10 @@
 package com.comcast.crm.objectrepository;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class Product {
+	@FindBy(xpath="//img[@alt='Create Product...']")
+	private WebElement createProducrImgBtn;	
 
 }
